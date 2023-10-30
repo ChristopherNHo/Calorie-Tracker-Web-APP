39af5a4571796b03e2a038dba9fcde23405d3d0a
LoganTadano <logan.tadano@gmail.com>
2023-10-30 00:05:24 -0700

    second push

--- routes.js
+++ routes.js
@@ -23,14 +23,35 @@ let params2 = {
 
 }
 
+fetch("https://jsonplaceholder.typicode.com/todos", {
+  method: "POST",
+  body: JSON.stringify({
+    userId: 1,
+    title: "Fix my bugs",
+    completed: false
+  }),
+  headers: {
+    "Content-type": "application/json; charset=UTF-8"
+  }
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
 
+  
+
+async function getData2(){
+const reponse = await fetch(api_url)
+const data = await reponse.json();
+console.log(data);
+
+
+}
 function getData(){
   return fetch(api_url).then(response => response.json())
 }
 
 let api_url = `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${encodeURIComponent(params.api_key)}&query=${encodeURIComponent(params.query)}&dataType=${encodeURIComponent(params.dataType)}&pagesize=${encodeURIComponent(params.pagesize)}`
-
-
+let api_url2 = `https://api.nal.usda.gov/fdc/v1/foods/search`;
 
 
 //getData().then(data=> console.log(data.foods[0].foodNutrients[0].value));
@@ -46,24 +67,30 @@ router.get("/",function(req,res) {
   res.sendFile(path.resolve(__dirname,"index.html"));
 });
 
- router.get("/food",function(req,res) {
+router.get("/food",function(req,res) {
   console.log("GET REQUEST")
 
-params.query = req.query.foodName;
-console.log(params.query);
-  let protein;
-  getData().then(
-    (data)=> {
-        console.log(data)
-        protein = data.results;
-        console.log(protein);
-        res.json({val:protein,name:params.query});
-       
-    }
-  )
+  params.query = req.query.foodName;
 
-  
-  
+  fetch(api_url2, {
+  method: "POST",
+  body: JSON.stringify({
+    "query" : params.query,
+    "dataType": [
+      "Foundation",
+      "Survey (FNDDS)"
+    ],
+    pagesize: 5,
+    pageNumber: 1
+  }),
+  headers: {
+    "Content-type": "application/json",
+
+  }
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+  res.json(null);
 });
 
 
