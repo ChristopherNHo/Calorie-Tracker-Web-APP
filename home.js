bc49a32071ad10d1cd572b638f4833c0373948c8
FallMarigold949 <117934764+FallMarigold949@users.noreply.github.com>
2023-10-30 13:36:19 -0700

    index.html script

--- home.js
+++ home.js
@@ -1,2 +1,40 @@
 
+		$(document).ready(function(){        
 
+
+				$(document).on('keypress',function(e) {
+
+					if(e.which == 13) {
+					
+						clicked();
+						
+						
+					}
+				});
+
+				}); 
+	function clicked()
+	{
+	/* console.log("CLICKED FUNCTIOn")
+	$.get("/food",function(data){
+		
+		console.log(data.protein);
+		//$("#food").append('<li>' +"protein " + data.protein + '</li>');
+	}); */
+
+
+	$.ajax({
+	url: "/food",
+	type: "GET",
+	data: {foodName:$("#search").val()},
+	success: add
+	});
+
+
+	}
+	function add(data){
+		console.log("success function")
+		console.log(data.name.foods[0]);
+		$("#food").append('<li>' + data.name.foods[0].description + " : " + data.name.foods[0].fdcId + '</li>');
+
+	}
