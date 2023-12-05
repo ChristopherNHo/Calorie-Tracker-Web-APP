48c31746e20448aab0497fe0358f48e70f7b042d
FallMarigold949 <117934764+FallMarigold949@users.noreply.github.com>
2023-12-05 12:37:59 -0800

    Update myDatabase.js

--- myDatabase.js
+++ myDatabase.js
@@ -10,11 +10,9 @@ let myDatabase = function() {
 
 myDatabase.prototype.postData = function(data,res) {
   let obj = {foodName:data.foodName,calories:data.calories,fats:data.fats,carbs:data.carbs,proteins:data.proteins,sugars:data.sugars};
-  DataModel.create(obj,function(error,info) {
-      if (error) {
-          return res.json({error:true});
-      }
-      return res.json({error:false});
+  DataModel.create(obj,function(info) {
+      
+      res.json();
 
   });
 }
