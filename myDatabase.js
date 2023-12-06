ee0b586db3bad1d932aea70d9b0f39b2e29690b3
FallMarigold949 <117934764+FallMarigold949@users.noreply.github.com>
2023-12-06 13:24:33 -0800

    Update myDatabase.js

--- myDatabase.js
+++ myDatabase.js
@@ -16,6 +16,30 @@ myDatabase.prototype.postData = function(data,res) {
 
   });
 }
+myDatabase.prototype.getData = function(totalIndex,res) {
+
+  DataModel.find({totalIndex:totalIndex},function(info,error) {   
+      console.log(info);
+      if (error) {
+          return res.json({error:true});
+      }
+      else if (info == null) {
+          return res.json({error:true});
+      }
+
+      if (info.length == 1)    
+      		return res.json({foodName:info[0].foodName,calories:info[0].calories,fats:info[0].fats,carbs:info[0].carbs,proteins:info[0].proteins,sugars:info[0].sugars,totalIndex:info[0].totalIndex});
+      else
+          return res.json({error:true});
+   });
+}
+myDatabase.prototype.putData = function(oldIndex,res) {
+  //let obj = {ident:data.ident,name:data.name,grade:data.grade,residence:data.residence};  
+  DataModel.findOneAndUpdate({totalIndex:oldIndex},
+    {totalIndex:(oldIndex-1)},function() {
+    	res.json();
+  });
+}
 
 myDatabase.prototype.deleteData = function(totalIndex,res) {
 	console.log(totalIndex);
